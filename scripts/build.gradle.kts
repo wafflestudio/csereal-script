fb1e79ee4d4f86b38d6d20d798e2bb12c06629d6
huGgW <whjoon0225@naver.com>
2025-03-22 00:11:27 +0900

    feat(scripts): add common used dependencies

--- scripts/build.gradle.kts
+++ scripts/build.gradle.kts
@@ -1,5 +1,20 @@
 allprojects {
     dependencies {
+        // jooq (sql abstraction)
+        implementation("org.jooq:jooq:3.20.2")
+        implementation("org.jooq:jooq-kotlin:3.20.2")
+
+        // db driver
+        runtimeOnly("com.mysql:mysql-connector-j:9.2.0")
+
+        // jsoup (html parser)
+        implementation("org.jsoup:jsoup:1.19.1")
+
+        // logger
+        implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
+        implementation("org.slf4j:slf4j-simple:2.0.3")
+        implementation("org.slf4j:slf4j-api:2.0.3")
+
         implementation(project(":generated"))
     }
 }
