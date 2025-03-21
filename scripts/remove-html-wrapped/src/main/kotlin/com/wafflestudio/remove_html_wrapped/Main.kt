e460fb955218897de1cd072a7d37d4f94098e79d
우혁준 (Logan) <whjoon0225@naver.com>
2025-03-22 01:12:47 +0900

    feat(scripts/remove html wrapped): <html></html> 제거 스크립트 (#3)
    * feat: init script
    
    * feat: add query execution script main function
    
    * fix: dynamic logger using table , field name
    
    * feat: make remove-html-wrapped executable

---
+++ scripts/remove-html-wrapped/src/main/kotlin/com/wafflestudio/remove_html_wrapped/Main.kt
@@ -0,0 +1,116 @@
+package com.wafflestudio.remove_html_wrapped
+
+import com.wafflestudio.generated.jooq.tables.references.NEWS
+import com.wafflestudio.generated.jooq.tables.references.NOTICE
+import com.wafflestudio.generated.jooq.tables.references.SEMINAR
+import com.wafflestudio.shared.db.mustGetDBConnection
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.jooq.*
+import org.jooq.exception.DataAccessException
+import org.jooq.impl.DSL
+import org.jooq.impl.TableImpl
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
+
+private val logger = KotlinLogging.logger {}
+
+fun main() {
+    val dbConn = mustGetDBConnection()
+
+    logger.info { "Start removing HTML tags..."}
+    dbConn.use {
+        val dslContext = DSL.using(it, SQLDialect.MYSQL)
+
+        // News
+        extractBodyQueryExecution(
+            dslContext,
+            NEWS,
+            NEWS.ID,
+            NEWS.DESCRIPTION,
+        )
+
+        // Seminar - description
+        extractBodyQueryExecution(
+            dslContext,
+            SEMINAR,
+            SEMINAR.ID,
+            SEMINAR.DESCRIPTION,
+        )
+
+        // Seminar - introduction
+        extractBodyQueryExecution(
+            dslContext,
+            SEMINAR,
+            SEMINAR.ID,
+            SEMINAR.INTRODUCTION,
+        )
+
+        // Seminar - additional note
+        extractBodyQueryExecution(
+            dslContext,
+            SEMINAR,
+            SEMINAR.ID,
+            SEMINAR.ADDITIONAL_NOTE,
+        )
+
+        // Notice
+        extractBodyQueryExecution(
+            dslContext,
+            NOTICE,
+            NOTICE.ID,
+            NOTICE.DESCRIPTION,
+        )
+    }
+}
+
+fun <R: Record, T: TableImpl<R>> extractBodyQueryExecution(
+    dslContext: DSLContext,
+    table: T,
+    idField: TableField<R, Long?>,
+    descriptionField: TableField<R, String?>,
+) {
+    logger.info { "Processing ${table.name} - ${descriptionField.name}..." }
+
+    val cursor = dslContext.selectFrom(table)
+        .whereHtmlClause(descriptionField)
+        .orderBy(idField.desc())
+        .fetchLazy()
+
+    cursor.forEach { col ->
+        val description = col.get(descriptionField.name) as String?
+        val id = col.get(idField.name) as Long?
+
+        try {
+            description?.let {
+                dslContext.update(table)
+                    .set(descriptionField, extractOnlyBody(it))
+                    .where(idField.eq(id))
+                    .execute()
+            }
+            logger.info { "Updated ${table.name} id: ${id}" }
+        } catch (e: DataAccessException) {
+            logger.error { "Failed to update ${table.name} id: ${id}, error: ${e.message}" }
+        }
+    }
+}
+
+fun <R: Record> SelectWhereStep<R>.whereHtmlClause(descriptionField: TableField<R, String?>): SelectConditionStep<R> {
+    return this.where(descriptionField.like("""<html>%"""))
+        .and(descriptionField.like("""%</html>"""))
+}
+
+fun extractOnlyBody(original: String): String {
+    val document = Jsoup.parse(original)
+    val bodies = document.select("body")
+
+    return when (bodies.size) {
+        0 -> document.html()
+        1 -> bodies.first()!!.html()
+        else -> {
+            val div = Element("div")
+            bodies.flatMap { it.children() }
+                .let { div.appendChildren(it) }
+                .outerHtml()
+        }
+    }
+}
